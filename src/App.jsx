e16f18b86c33d5299a77023417f6fe9e0451eceb
g-akca <gok.akca66@gmail.com>
2026-07-11 11:34:35 +0300

    Give App styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import MainSection from "./components/MainSection";
 
 function App() {
   return (
-    <div>
+    <div className="text-base leading-base text-neutral-700 min-h-screen bg-neutral-100 flex flex-col gap-5">
       <AppBar />
 
       <MainSection />
