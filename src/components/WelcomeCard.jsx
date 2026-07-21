71b394487d1570d0b9001029a103bac5d2425df3
g-akca <gok.akca66@gmail.com>
2026-07-21 18:08:28 +0300

    Give WelcomeCard content and styles

--- src/components/WelcomeCard.jsx
+++ src/components/WelcomeCard.jsx
@@ -1,7 +1,37 @@
+import sunIcon from "/images/icon-sun.svg";
+
 function WelcomeCard() {
   return (
-    <>
-    </>
+    <div 
+      className="
+        bg-[radial-gradient(circle_at_100%_0%,var(--color-terracotta-400)_0%,var(--color-terracotta-500)_50%,var(--color-terracotta-700)_100%)] 
+        p-6 rounded-[20px] text-sun-50 flex flex-col gap-5 justify-between
+      "
+    >
+      <div className="flex flex-col gap-5">
+        <div className="pt-4 flex justify-between items-start border-t border-t-terracotta-400 border-dashed">
+          <h2 className="text-[10px] leading-base tracking-[1px] font-mono uppercase">Welcome Card</h2>
+
+          <img src={sunIcon} alt="Sun icon" className="w-12.25 aspect-square" />
+        </div>
+
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-2">
+            <p className="text-sun-200 text-[20px] leading-base italic font-fraunces tracking-[-0.1px]">A note from your host,</p>
+
+            <p className="text-[40px] leading-[120%] italic font-fraunces tracking-[-0.5px]">Margaux.</p>
+          </div>
+
+          <p>We're so glad you're coming. The shutters will be open, the lemonade cold, and the cat - Poivre - pretending not to notice you.</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <p className="text-[10px] leading-base tracking-[1px] font-mono uppercase">Room</p>
+
+        <p className="text-[20px] leading-base tracking-[-0.1px] font-fraunces">La Garrigue</p>
+      </div>
+    </div>
   )
 }
 
