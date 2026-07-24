bed6297bced16e49372df155580621396121196e
g-akca <gok.akca66@gmail.com>
2026-07-24 10:32:40 +0300

    Update ReceiptCard and WelcomeCard styles for rotation

--- src/components/ReceiptCard.jsx
+++ src/components/ReceiptCard.jsx
@@ -4,7 +4,7 @@ function ReceiptCard() {
   return (
     <div 
       className="
-        bg-neutral-50 p-6 rounded-[20px] text-neutral-900 flex flex-col gap-5 justify-between
+        bg-neutral-50 p-6 rounded-[20px] text-neutral-900 flex flex-col gap-5 justify-between mx-2 my-1.5 -rotate-2
         shadow-[0_1px_rgba(0,0,0,0.03),0_16px_30px_-20px_rgba(62,44,30,0.35),0_20px_40px_-30px_rgba(62,44,30,0.16)]
       "
     >
