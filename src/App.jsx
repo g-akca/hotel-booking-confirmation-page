cea526164faf4df9846f16a719fb05b71b533420
g-akca <gok.akca66@gmail.com>
2026-08-02 18:20:40 +0300

    Create Menu component and integrate it into App for conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import AppBar from "./components/AppBar";
 import MainSection from "./components/MainSection";
+import Menu from "./components/Menu";
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className="text-base leading-base text-neutral-700 min-h-screen bg-neutral-100 flex flex-col gap-5">
       <AppBar />
 
       <MainSection />
+
+      {isMenuOpen && (
+        <Menu />
+      )}
     </div>
   )
 }
