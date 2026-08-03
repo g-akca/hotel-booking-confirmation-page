3d648ed4a766f89dac4e156b9707c622111becb7
g-akca <gok.akca66@gmail.com>
2026-08-03 19:58:14 +0300

    Add border to Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,6 @@
 function Menu() {
   return (
-    <div className="fixed z-20 left-0 right-13.75 inset-y-0 bg-neutral-100">
+    <div className="fixed z-20 left-0 right-13.75 inset-y-0 bg-neutral-100 border border-neutral-400">
       
     </div>
   )
