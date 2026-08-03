e0dfef88ca0c43eebc3e26a42f9e21214e8cf73c
g-akca <gok.akca66@gmail.com>
2026-08-03 19:53:36 +0300

    Refactor App component layout and update Menu component structure

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div className="text-base leading-base text-neutral-700 relative min-h-screen bg-neutral-100 flex flex-col gap-5">
+    <div className="text-base leading-base text-neutral-700 min-h-screen bg-neutral-100 flex flex-col gap-5">
       <AppBar openMenu={() => setIsMenuOpen(true)} />
 
       <MainSection />
@@ -17,7 +17,7 @@ function App() {
           <button 
             type="button" 
             onClick={() => setIsMenuOpen(false)}
-            className="absolute z-10 inset-0 bg-neutral-900/80" 
+            className="fixed z-10 inset-0 bg-neutral-900/80" 
           />
           
           <Menu />
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,8 @@
 function Menu() {
   return (
-    <>
-    </>
+    <div className="fixed z-20 left-0 right-13.75 inset-y-0 bg-neutral-100">
+      
+    </div>
   )
 }
 
