639a24249b2f5bd97cbc5caafde4b9e4d7d7e520
g-akca <gok.akca66@gmail.com>
2026-08-05 15:08:26 +0300

    Add main Menu component structure

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,25 @@
 function Menu() {
   return (
-    <div className="fixed z-20 left-0 right-13.75 inset-y-0 bg-neutral-100 border border-neutral-400">
-      
+    <div className="fixed z-20 left-0 right-13.75 inset-y-0 bg-neutral-100 border border-neutral-400 p-4 flex flex-col justify-between gap-4">
+      <div className="flex flex-col gap-4">
+        <div className="pb-4.25 flex justify-between items-center">
+
+        </div>
+
+        <div className="flex flex-col gap-0.5">
+
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-5">
+        <div className="rounded-2xl bg-sun-300 px-4 py-3 flex flex-col gap-1.5 shadow-[0_1px_rgba(0,0,0,0.04),0_10px_24px_-16px_rgba(194,90,46,0.4)]">
+
+        </div>
+
+        <div className="pt-4 flex flex-col gap-2.5">
+
+        </div>
+      </div>
     </div>
   )
 }
