c906753acba4527ae8fae7b439ded7af05cdc6c1
g-akca <gok.akca66@gmail.com>
2026-08-06 17:30:02 +0300

    Add header part of Menu, create navList array

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,45 @@
+import logoImg from "/images/logo.svg";
+import closeIcon from "/images/icon-close.svg";
+
+const baseUrl = import.meta.env.BASE_URL;
+
+const navList = [
+  {
+    icon: `${baseUrl}images/icon-bed.svg`,
+    label: "Your stay",
+    isSelected: true,
+  },
+  {
+    icon: `${baseUrl}images/icon-house.svg`,
+    label: "The house",
+  },
+  {
+    icon: `${baseUrl}images/icon-pin.svg`,
+    label: "Around town",
+  },
+  {
+    icon: `${baseUrl}images/icon-breakfast-outline.svg`,
+    label: "Breakfast",
+  },
+  {
+    icon: `${baseUrl}images/icon-mail.svg`,
+    label: "Messages",
+  },
+];
+
 function Menu() {
   return (
     <div className="fixed z-20 left-0 right-13.75 inset-y-0 bg-neutral-100 border border-neutral-400 p-4 flex flex-col justify-between gap-4">
       <div className="flex flex-col gap-4">
-        <div className="pb-4.25 flex justify-between items-center">
+        <div className="pb-4.25 flex justify-between items-center border-b border-neutral-400">
+          <img src={logoImg} alt="Logo image" />
 
+          <button 
+            type="button" 
+            className="w-8 aspect-square rounded-sm border border-neutral-400 flex justify-center items-center"
+          >
+            <img src={closeIcon} alt="" />
+          </button>
         </div>
 
         <div className="flex flex-col gap-0.5">
