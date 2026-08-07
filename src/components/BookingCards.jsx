69d69423c91660a7698db25a7a801282a04c6a29
g-akca <gok.akca66@gmail.com>
2026-08-07 16:59:22 +0300

    Refactor BookingCards, WelcomeCard, and ReceiptCard for improved layout and responsiveness

--- src/components/BookingCards.jsx
+++ src/components/BookingCards.jsx
@@ -3,7 +3,12 @@ import ReceiptCard from "./ReceiptCard";
 
 function BookingCards() {
   return (
-    <div className="grid grid-rows-2 px-2 py-1.5 gap-1.5 tablet:px-6 tablet:py-8 tablet:gap-0 tablet:grid-rows-none tablet:grid-cols-2">
+    <div 
+      className="
+        px-2 py-1.5 grid grid-rows-2 justify-items-center gap-1.5 tablet:px-6 
+        tablet:py-8 tablet:gap-0 tablet:grid-rows-none tablet:grid-cols-2
+      "
+    >
       <WelcomeCard />
 
       <ReceiptCard />
