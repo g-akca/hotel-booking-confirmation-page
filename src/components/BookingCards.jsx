5ae9fccce03a82f9447d6c3ed325e1c179d8cb0c
g-akca <gok.akca66@gmail.com>
2026-08-07 16:51:19 +0300

    Add tablet responsive styles to BookingCards, WelcomeCard, and ReceiptCard

--- src/components/BookingCards.jsx
+++ src/components/BookingCards.jsx
@@ -3,7 +3,7 @@ import ReceiptCard from "./ReceiptCard";
 
 function BookingCards() {
   return (
-    <div className="grid grid-rows-2">
+    <div className="grid grid-rows-2 px-2 py-1.5 gap-1.5 tablet:px-6 tablet:py-8 tablet:gap-0 tablet:grid-rows-none tablet:grid-cols-2">
       <WelcomeCard />
 
       <ReceiptCard />
